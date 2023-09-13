509cc2a9523ee4310bff0b1fe3ccdb2af6d426d3
ahmedqqp9 <mbbd3999#hotmail.com>
2023-09-12 17:45:56 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * print_alphabet - wirte the the lower-case alphabet on a line.
- *
- *
- *
+ * print_alphabet - print all alphabet in lowercase
  */
+
 void print_alphabet(void)
 {
-	char c;
+	char letter;
 
-	for (c = 'a' ; c <= 'z' ; c++)
-	{
-		_putchar(c);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
 }
